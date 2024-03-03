5036be14d094601e17088727a1937e368b7355a9
rodgerswisdom <trulyhawona@gmail.com>
2024-03-03 03:43:00 +0300

    Rodgers function added

--- camp_1/src/App.js
+++ camp_1/src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Button(){
+  return(
+    <button>I'm a button</button>
+  )
+}
+
+const Rodgers = ()=> {
+  return(
+    <div>
+      <h1> Rodgers</h1>
+      <p>Part of the few, one of the best</p>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          <Button  onClick = {Rodgers} />
+        </h1>
         <p>
           Hello world
         </p>
